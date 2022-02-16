b6fba7eb7870fa82db85e90610a4ffe745b7c505
edsonmalagodi <edson.malagodi12@gmail.com>
2022-02-15 22:06:19 -0300

    build on vercel

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import estilos from '../pages/Styles.module.css'
+
 
 export default function Home() {
   return (
